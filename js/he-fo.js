4cce10cb17d474fa97f2a11a0ea08b0b7db27177
Web Creation Studios (WCS) <solutionsforyourweb@gmail.com>
2026-06-08 13:38:57 -0400

    WCS: Update he-fo.js

--- js/he-fo.js
+++ js/he-fo.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         <li><a href="pricing.html">Pricing</a></li>
                         <li><a href="contact.html">Contact</a></li>
                     </ul>
-                    <a href="event-planner.html">AI Planner</a>
+                   <a href="event-planner.html" class="nav-button">AI Planner</a>
                 </div>
             </nav>
         `;
